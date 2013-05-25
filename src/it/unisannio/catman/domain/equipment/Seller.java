db4f3397196fb90146a995a0687ff1ba543113f6
Danilo Iannelli <daniloiannelli6@gmail.com>
2013-05-25 19:11:12 +0200

    Merge

--- src/it/unisannio/catman/domain/equipment/Seller.java
+++ src/it/unisannio/catman/domain/equipment/Seller.java
@@ -1,12 +1,11 @@
 package it.unisannio.catman.domain.equipment;
 
+import java.util.List;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.Version;
+import javax.persistence.OneToMany;
 
 
 @Entity
--- src/it/unisannio/catman/domain/equipment/client/SupplierProxy.java
+++ src/it/unisannio/catman/domain/equipment/client/SupplierProxy.java
@@ -5,6 +5,7 @@ import it.unisannio.catman.domain.equipment.Supplier;
 import java.util.List;
 
 import com.google.web.bindery.requestfactory.shared.EntityProxy;
+import com.google.web.bindery.requestfactory.shared.ExtraTypes;
 import com.google.web.bindery.requestfactory.shared.ProxyFor;
 
 @ProxyFor(Supplier.class)
