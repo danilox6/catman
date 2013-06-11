064db00952fa4282bb798c21616559f88b9e2979
Danilo Iannelli <daniloiannelli6@gmail.com>
2013-06-11 19:03:38 +0200

    MockEnPersister: Assegnata una qualifica a Francesco, ora è un sommelier

--- src/it/unisannio/catman/common/client/MockEntityPersister.java
+++ src/it/unisannio/catman/common/client/MockEntityPersister.java
@@ -297,7 +297,7 @@ public class MockEntityPersister {
 														WorkerRequest workers = dataStore.workers();
 														workers.addQualification(qualification1).using(worker1);
 														workers.addQualification(qualification2).using(worker7);
-														workers.addQualification(qualification3).using(worker8);
+														workers.addQualification(qualification3).using(worker9);
 														workers.addQualification(qualification4).using(worker10);
 														workers.addQualification(qualification2).using(worker1);
 														workers.addQualification(qualification4).using(worker5);
